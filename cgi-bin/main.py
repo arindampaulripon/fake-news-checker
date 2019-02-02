a58ecb69c4bad819503cc66eb927976f4140b9b9
Arindam Paul <arindam017@gmail.com>
2019-02-03 01:01:17 +0600

    Update main.py

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -1,8 +1,10 @@
-﻿#!C:/Users/i7/Anaconda3/python.exe
+#!C:/Users/i7/Anaconda3/python.exe
 import cgi, cgitb
 import string
 import requests 
 from bs4 import BeautifulSoup 
+
+cgitb.enable()
 
 form = cgi.FieldStorage()
 
@@ -16,39 +18,77 @@ print("<head>")
 
 print("<link rel='stylesheet' type='text/css' href='../EasterSS.css'/></head>")
 print("<body>")
-
-print ("Hello.")
-
+print("<h2>")
+print ("Hi, Now you're seeing the top 100 google result at once")
+print("</h2>")
+print("</br>")
 q = form.getvalue('search')
 escaped_search_term = q
 
 number_results = 100
 language_code = 'en'
+print("<h2>")
+print ("You Searched For ",q)
+print("</h2>")
 
+print("</br>")
 
 URL = "https://www.google.com/search?q={}&num={}&hl={}".format(escaped_search_term, number_results, language_code)
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html.parser')
-
-
+print("<h2>")
+print("<b>")
+print("Google Search Result : ")
+print("</b>")
+print("</br>")
+print("</h2>")
 for node in soup.findAll('h3'):
     rs1 =  (''.join(node.findAll(text=True))).encode("utf-8")
    
     
-    
+    print("<ul>")
+    print("<li>")
     print(rs1)
-    print("<br \>")
+    print("</li>")
+    print("</ul>")
+    print("</br>")
+    
 
+    
 url3 = ('https://www.youtube.com/results?search_query='+q)
 r3 = requests.get(url3)
 soup3 = BeautifulSoup(r3.content, 'html.parser') 
-    
+print("<h2>")
+print("<b>")
+print("Youtube Video Result : ")
+print("</b>")
+print("</h2>")
+print("</br>")
 for node in soup3.find_all("h3"):
 
-    
+    print("<ul>")
+    print("<li>")
     print (''.join(node.findAll(text=True)))
-    print("<br\>")
+    print("</li>")
+    print("</ul>")
+    print("</br>")
     
+    
+print("<h2>")
+print("<b>")
+print("Google Search Result's Links : ")
+print("</b>")
+print("</br>")
+print("</h2>")
+
+
+for node in soup.findAll('cite'):
+    print("<ul>")
+    print("<li>")
+    print (''.join(node.findAll(text=True))).encode("utf-8")
+    print("</li>")
+    print("</ul>")
+    print("</br>")   
     
 
 
